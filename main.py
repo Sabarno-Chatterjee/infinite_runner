09e758435a72262f3164944195ca01cbee93176a
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-23 21:34:18 +0530

    initial layout

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+pygame.init()
+
+# GAME CONSTANTS(IN RGB):
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+WIDTH = 450
+HEIGHT = 300
+
+# GAME VARIABLES:
+score = 0
+player_x = 50
+player_y = 200
