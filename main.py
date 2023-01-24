1b0ba10a4f1410ba892fe2a1b6c4a5b071da2862
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-24 09:11:58 +0530

    set up timer and event handling in case of pygame.QUIT

--- main.py
+++ main.py
@@ -10,7 +10,29 @@ BLACK = (0, 0, 0)
 WIDTH = 450
 HEIGHT = 300
 
+
 # GAME VARIABLES:
 score = 0
 player_x = 50
 player_y = 200
+
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption("Infinite Runner")
+background = "black"
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 16)
+timer = pygame.time.Clock()
+
+running = True
+
+while running:
+    timer.tick(fps)
+    screen.fill(background)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+
+
